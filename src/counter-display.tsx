2bf64bcc31201a7e59c52781331b5a295df77331
Parthasarathi Das <parthasarathi.das@walmart.com>
2023-04-05 16:14:30 -0700

    add new changes to test PR review

--- src/counter-display.tsx
+++ src/counter-display.tsx
@@ -2,8 +2,10 @@ import { useCount } from "./counter-context";
 
 export default function CounterDisplay() {
   const { state } = useCount();
-  console.log(state);
   return (
-    <p>{state.count}</p>
+    <>
+      <h2>Counter Display</h2>
+      <p>{state.count}</p>
+    </>
   );
 }
